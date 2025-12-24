fb42936d61f978d11982041f865c7891e3f00e55
Henry Mak <henrymak5@hotmail.com>
2025-12-24 00:31:36 +0000

    Built out the staging models for the docking stations =("stg_tfl__docking_station.sql") and the journey's ("stg_tfl__journey.sql"). This including making sure the data types were appropriate and that the columns were renamed. After, I added some basic tests to the staging tables via "_stg_tfl.yml". More to be added later on with dbt expectations/dbt utils perhaps, and more tests added once I get a chance to take a look at the raw data to verify.

---
+++ models/staging/stg_tfl__journey.sql
@@ -0,0 +1,25 @@
+WITH source AS
+(
+    SELECT *
+    FROM {{ source('tfl', 'journey') }}
+),
+
+renamed AS
+(
+    SELECT
+        CAST(`Number` AS int64) AS journey_id,
+        TIMESTAMP(`Start date`) AS started_on_utc,
+        TIMESTAMP(`End date`) AS ended_on_utc,
+        CAST(`Start station number` AS int64) AS start_station_id,
+        `Start station` AS start_station_name,
+        CAST(`End station number` AS int64) AS end_station_id,
+        `End station` AS end_station_name,
+        CAST(`Bike number` AS int64) AS bike_id,
+        `Bike model` AS bike_model,
+        `Total duration` AS journey_duration_hr_min_string,
+        ROUND((CAST(`Total duration in ms` AS int64) / 1000), 1) AS journey_duration_seconds
+    FROM source
+)
+
+SELECT *
+FROM renamed
